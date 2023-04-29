979b66186b3e9440839d87b386ac9b287b3b5d19
Manuel Thomsen <masotoma@hotmail.com>
2023-04-29 13:57:02 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://skelegonDK.github.io',
   base: '/portfolio',
